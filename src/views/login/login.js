a7678fd5e1800ba3234df7a0ee5f830b8d5c89d3
Elano Bernardo <elanobernardo1@gmail.com>
2026-05-01 09:45:02 +0100

    implementacao de login com fetch no js completa

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,19 +1,38 @@
-const alertaErro = document.querySelector('.mostrar');
-const username = document.querySelector('#user');
-const senha = document.querySelector('#senha');
-const formLogin = document.querySelector('#form');
+const alertaErro = document.querySelector(".notificacao");
+const load = document.querySelector(".load");
+const username = document.querySelector("#user");
+const senha = document.querySelector("#senha");
+const formLogin = document.querySelector("#form");
+const URL = "http://localhost:3000/index/usuario/login";
 
+//funcao para fazer login
+const fazerLogin = async (data) => {
+  try {
+    const response = await fetch(URL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
+    if (response.ok) {
+      load.classList.toggle("mostrar-load");
+      (response) => response.json();
+      return console.log(response);
+    }
+    alertaErro.classList.toggle("mostrar");
+  } catch (e) {
+    console.log("erro ao tentar logar " + e);
+  }
+};
 
 //formulario login
-formLogin.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    data={
-        nome:username.value,
-        senha:senha.value
-     }
-     console.log(data);
-
-})
-
-
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  data = {
+    username: username.value,
+    senha: senha.value,
+  };
+  fazerLogin(data);
+});
