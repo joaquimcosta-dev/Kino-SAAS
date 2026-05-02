9c843618f2fd841ad9eb5ecd07d8e3f7ec2e8535
Elano Bernardo <elanobernardo1@gmail.com>
2026-05-02 13:53:18 +0100

    mudança no visiual da pagina reclamacao e no script dele

--- src/views/reclamacao/reclamacao.js
+++ src/views/reclamacao/reclamacao.js
@@ -1,31 +1,65 @@
-document.getElementById('complaintForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+const nome = document.querySelector('#nome');
+const telefone = document.querySelector('#nume');
+const motivo =document.querySelector('#selecaoReclamacao');
+const formulario = document.getElementById('complaintForm');
+const btn = document.getElementById('submitBtn');
+const mensagem = document.getElementById('message');
+const URL = "http://localhost:3000/index/reclamacao/enviar";
 
-
-    const message = document.getElementById('message').value;
-    const btn = document.getElementById('submitBtn');
-
-
-    if (message.trim() === "") {
+// funcao para envio de reclamacao para o backend
+const enviarRclamacao= async(data)=>{
+    try {
+        const res = await fetch(URL,{
+            method:"post",
+            headers:{
+                "Content-Type":"application-json",
+                body:JSON.stringify(data)
+            }
+        })
+        //fazendo a verificacao do envio
+         if(res.ok){
+            alert("Reclamação enviada com sucesso!");
+            return;
+            }
+             alert("Reclamação não enviada!");
+              return;
+        
+    } catch (e) {
+        console.log("Erro ao tentar enviar a reclamação")
+    }
+}
+//funcao para limpar os campos
+const limpar=()=>{
+    nome.value=''
+    telefone.value=''
+    motivo.value=''
+    mensagem.value=''
+    btn.innerText = "enviar";
+    btn.disabled = false;
+}
+formulario.addEventListener('submit', (e)=> {
+    if (mensagem.value.trim() === "") {
         alert("Por favor, Digite a sua Reclamação antes de enviar.");
         return;
     }
-
-
-    // Efeito visual de envio
+    e.preventDefault();
+    //pegados os valores enviados
+    const data ={
+        nome:nome.value,
+        telefone:telefone.value,
+        motivo:motivo.value,
+        descricao:mensagem.value
+    }
     btn.innerText = "Enviando...";
-    btn.disabled = true;
+     btn.disabled = true;
+    //chamando a funcao enviar reclamacao
+   enviarRclamacao(data);
+    // chamando a funcao limpar
+    limpar();
+   
 
 
-    // Simulação de uma requisição API (backend)
-    setTimeout(() => {
-        console.log("Mensagem enviada:", message);
+
         
-        alert("Reclamação enviada com sucesso!");
-        
-        // Limpar formulário
-        document.getElementById('message').value = "";
-        btn.innerText = "enviar";
-        btn.disabled = false;
-    }, 1500);
+    
 });
