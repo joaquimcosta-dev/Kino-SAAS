68a3dd7585b30eec5647467dcb5ef465a71c8170
Elano Fernando Bernardo <elanoskill@gmail.com>
2026-05-15 10:23:24 +0100

    alterando o model reclamacao na funcao deletar e editar
    alterando o model reclamacao na funcao deletar e editar

--- Epre/src/model/reclamacao.js
+++ Epre/src/model/reclamacao.js
@@ -1,17 +1,17 @@
 import db from '../config/db.js';
 
-//Função paara atualizar uma reclamação existente na db
+/*Função paara atualizar uma reclamação existente na db
 export const atualizarReclamacao = async({id,data}) =>{
     const{nome,motivo,descricao,data_reclamacao,telefone}=data;
     const atualizado = await db.query("update reclamacao SET nome=?, motivo=?, descricao=?, data=?,telefone=? WHERE id_reclamacao=?",
         [nome,motivo,descricao,data_reclamacao,telefone,id]
     );
     return atualizado;
-}
+}*/
 
 //Função para buscar uma unica reclamação
 export const listarReclamacaoPorld = async (id) => {
-    const reclamacao = await db.query("SELECT * FROM reclamacao WHERE id _reclamacao =?", [id]); return reclamacao[0];
+    const reclamacao = await db.query("SELECT * FROM reclamacao WHERE id_recla =?", [id]); return reclamacao[0] || null;
 }
 
 
@@ -21,11 +21,12 @@ export const criar_reclamacao = async (data)=>{
     const reclamacao= await db.query("insert into reclamacao (nome,motivo,descricao,data,telefone) values(?,?,?,?,?)",[nome,motivo,descricao,datas,telefone])
     return reclamacao;
 }
-//Funcao que contemm o sql para deletar reclamacao no db
+/*
+// Funcao que contemm o sql para deletar reclamacao no db
 export const deletarRelclamacao= async(id)=>{
     const eliminado = await db.query("delete from reclamacao where id_reclamacao =?",[id]);
     return eliminado;
-}
+}*/
 //Funcao que contemm o sql para listar reclamacao no db
 export const listarTodasReclamacao = async()=>{
     const lista = await db.query("select *from reclamcao");
