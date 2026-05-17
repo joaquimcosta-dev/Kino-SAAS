cb66cc8408cef8f50391535fd03503bbcb7578bc
Elano Bernardo <elanobernardo1@gmail.com>
2026-05-17 21:21:14 +0100

    merge

--- Epre/src/controllers/controller_produto.js
+++ Epre/src/controllers/controller_produto.js
@@ -1,39 +1,44 @@
 import express from "express";
 import * as service from "../service/service_produto.js";
-import  { permissaoAdmin} from '../middlewares/auth.js';
+import { permissaoAdmin } from "../middlewares/auth.js";
 
 const controller = express.Router();
 
 //rota listar produtos
-controller.get("/listar", async (req, res)=>{
-    try{
+controller.get("/listar", async (req, res) => {
+    try {
         const produtos = await service.listarProdutos();
         return res.status(200).json(produtos);
     } catch (e) {
         console.log(e);
-        return res.status(400).json({ menssagem:"Erro ao listar produtos" });
+        return res.status(400).json({ menssagem: "Erro ao listar produtos" });
     }
 });
 
 //rota para cadastrar produto
-controller.post("/cadastrar",permissaoAdmin, async(req, res)=>{
-    try{
-        const { nome, img, preco, descricao, id_cat} = req.body;
+controller.post("/cadastrar", permissaoAdmin, async (req, res) => {
+    try {
+        const { nome, img, preco, descricao, id_cat } = req.body;
         const id_user = req.user.id;
         const resultado = await service.cadastrarProduto(
-            nome, img, preco, descricao, id_cat, id_user 
+            nome,
+            img,
+            preco,
+            descricao,
+            id_cat,
+            id_user
         );
         return res.status(201).json(resultado);
     } catch (e) {
         console.log(e);
-        return res.status(400).json({ menssagem:"Erro de cadastro" });
+        return res.status(400).json({ menssagem: "Erro de cadastro" });
     }
 });
 
 //rota pra buscar produto
-controller.get("/listar/:id", async(req, res)=>{
-    try{
-        const {id} = req.params.id;
+controller.get("/listar/:id", async (req, res) => {
+    try {
+        const { id } = req.params.id;
 
         //verifica se é um número válido
         if (isNaN(id)) {
@@ -45,14 +50,16 @@ controller.get("/listar/:id", async(req, res)=>{
             return res.status(400).json({ mensagem: "ID inválido" });
         }
 
-        const produto = await service.buscarProdutoId(id)
+        const produto = await service.buscarProdutoId(id);
 
         return res.status(200).json(produto);
-    }catch(e){
+    } catch (e) {
         console.log(e);
-        return res.status(404).json({ menssagem:"Erro ao encontrar o produto" });
+        return res
+            .status(404)
+            .json({ menssagem: "Erro ao encontrar o produto" });
     }
-})
+});
 
 //rota atualizar produto
 controller.put("/atualizar/:id", permissaoAdmin, async (req, res) => {
@@ -64,40 +71,43 @@ controller.put("/atualizar/:id", permissaoAdmin, async (req, res) => {
             return res.status(400).json({ mensagem: "Id inválido" });
         }
 
-        
         const { nome, img, preco, deescricao, id_cat } = req.body;
 
         const resultado = await service.atualizarProduto(
-            id, nome, img, preco, deescricao, id_cat
+            id,
+            nome,
+            img,
+            preco,
+            deescricao,
+            id_cat
         );
 
         return res.status(200).json(resultado);
     } catch (e) {
         console.log(e);
-        return res.status(400).json({ mensagem: "erro ao atualizar o produto"});
+        return res
+            .status(400)
+            .json({ mensagem: "erro ao atualizar o produto" });
     }
 });
 
 //rota Eliminar produto
 
-controller.delete("/eliminar/;id", permissaoAdmin, async(req, res)=>{
-    try{
+controller.delete("/eliminar/;id", permissaoAdmin, async (req, res) => {
+    try {
         const id = req.params.id;
 
         //verifica o id
-        if(isNaN(id) || id <= 0){
-            return res.status(400).json({mensagem: "id invalido"});
+        if (isNaN(id) || id <= 0) {
+            return res.status(400).json({ mensagem: "id invalido" });
         }
 
         const eliminar = service.eliminarProduto(id);
         return res.status(200).json(eliminar);
-
-    }catch(e){
+    } catch (e) {
         console.log(e);
-        return res.status(400).json({mensagem: "erro ao eliminar o produto"});
-    }   
-
-
-})
+        return res.status(400).json({ mensagem: "erro ao eliminar o produto" });
+    }
+});
 
 export default controller;
