792920bf89e031ad0b98b9c53e94c1170d8adf50
Elano Bernardo <elanobernardo1@gmail.com>
2026-05-18 06:07:03 +0100

    resolvendo bug do produto

--- Epre/src/model/categoria.js
+++ Epre/src/model/categoria.js
@@ -2,10 +2,10 @@ import db from "../config/db.js";
 //funcao para casdastrar categoria
 export const criarCategoria = async date => {
     const { nome } = data;
-    const novaCat = await db.query("insert into categoria(nome) values (?)", [
+    const [novaCat] = await db.query("insert into categoria(nome) values (?)", [
         nome
     ]);
-    return novaCat;
+    return novaCat[0];
 };
 
 //listar todas as categirias
@@ -15,23 +15,27 @@ export const listar = async () => {
 };
 
 //funcao para buscar por id
-export const listarId = async (id) => {
-    const [buscado] = await db.query("select nome from  categoria where id_cat =?", [
-        id
-    ]);
+export const listarId = async id => {
+    const [buscado] = await db.query(
+        "select nome from  categoria where id_cat =?",
+        [id]
+    );
+    return buscado[0] || null;
 };
 
-
 //funcao para deletar
-export const deletar = async (id) => {
-    const deletado = await db.query("delete from  categoria where id_cat =?", [
-        id
-    ]);
+export const deletar = async id => {
+    const [deletado] = await db.query(
+        "delete from  categoria where id_cat =?",
+        [id]
+    );
+    return deletado[0];
 };
 //funcao para atualizar categirias
 export const atualizar = async ({ id, nome }) => {
-    const atualizado = await db.query(
+    const [atualizado] = await db.query(
         "update categoria set nome =? where id =?",
         [nome, id]
     );
+    return atualizado[0];
 };
--- Epre/src/model/produto.js
+++ Epre/src/model/produto.js
@@ -1,33 +1,41 @@
 import db from "../config/db.js";
 
 //função listar produtos pela db
-export const listarProdutos = () =>{
-    return db.promise().query("SELECT * FROM produto")
+export const listarProdutos = () => {
+    return db.promise().query("SELECT * FROM produto");
 };
 //função procurar produto pela db
-export const buscarProdutoId = (id)=>{
-    return db.promise().query(
-        "SELECT * FROM produto WHERE id_prod = ?",[id]
-    );
+export const buscarProdutoId = id => {
+    return db.promise().query("SELECT * FROM produto WHERE id_prod = ?", [id]);
 };
 //função criar produto pela db
-export const cadastrarProduto = (id_prod, nome, img, preco, descricao, id_user, id_cat) =>{
-    return db.promise().query(
-        "INSERT INTO produto (id_prod, nome, img, preco, descricao, id_user, id_cat) VALUES (?, ?, ?, ?, ?, ?, ?)",
-        [id_prod, nome, img, preco, descricao, id_user, id_cat]
-    );
-}
+export const cadastrarProduto = ({
+    id_prod,
+    nome,
+    img,
+    preco,
+    descricao,
+    id_user,
+    id_cat
+}) => {
+    return db
+        .promise()
+        .query(
+            "INSERT INTO produto (id_prod, nome, img, preco, descricao, id_user, id_cat) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [nome, img, preco, descricao, id_user, id_cat]
+        );
+};
 //função atualizar produto pela db
-export const atualizarProduto = (id, nome, img, preco, descricao, id_cat) =>{
-    return db.promise().query(
-        "UPDATE produto SET nome=?, img=?, preco=?, descricao=?, id_cat=? WHERE id_prod=?",
-        [nome, img, preco, descricao, id_cat, id]
-    );
-}
+export const atualizarProduto = (id, nome, img, preco, descricao, id_cat) => {
+    return db
+        .promise()
+        .query(
+            "UPDATE produto SET nome=?, img=?, preco=?, descricao=?, id_cat=? WHERE id_prod=?",
+            [nome, img, preco, descricao, id_cat, id]
+        );
+};
 
 //função deletar produto pela db
-export const eliminarProduto = (id)=>{
-    return db.promise().query(
-        "DELETE FROM produto WHERE id_prod=?" [id],
-    );
-}
+export const eliminarProduto = id => {
+    return db.promise().query("DELETE FROM produto WHERE id_prod=?"[id]);
+};
