ff90870dade0db73ff79c589b8b621c9cda27332
joaquimcosta-dev <joaquimdacosta1805@gmail.com>
2026-06-05 00:00:19 +0100

    dando erro de campo obrigatorio

--- backend/src/controllers/controller_produto.js
+++ backend/src/controllers/controller_produto.js
@@ -19,26 +19,19 @@ controller.get("/listar", async (req, res) => {
             
 //rota para cadastrar produto
 controller.post("/cadastrar", auth, permissaoAdmin, async (req, res) => {
+    
     try {
-        const { nome, img, preco, descricao, requerQtd, id_cat } = req.body;
-        //buscar categoria
-        //const id_cat = req.params.id;
-        //const cat = await servicoCategoria.buscarCatId(id_cat);
-        //verificar se a categoria existe
-        /*  if (!cat) {
-            return res
-            .status(404)
-            .json({ message: "categoria não encontrado" });
-            //return res.status(404).json({message:"Categoria não econtrado"});
-            }*/
 
-        if (!req.body.nome || !req.body.preco || !req.body.img || !req.body.descricao) {
-            console.log(message)
+        const { nome, img, preco, descricao, requerQtd, id_cat } = req.body;
+        
+        if (!req.body.nome || !req.body.preco || !req.body.img || !req.body.descricao || !req.body.id_cat) {
+            
             return res.status(400).json({ message: "Campo obrigatório" });
         }
 
         //pegar o id do usuario logado
         const id_user = req.user.id;
+
         if (!id_user) {
             return res.status(401).json({ message: "Usuario não autenticado" })
         }
@@ -53,7 +46,8 @@ controller.post("/cadastrar", auth, permissaoAdmin, async (req, res) => {
             id_user
         });
 
-        return res.status(201).json(resultado);
+        return res.status(201).json(nome, preco, descricao, requerQtd, id_cat, id_user);
+
     } catch (e) {
         console.log(e);
         return res.status(500).json({ menssagem: "Erro de cadastro" });
