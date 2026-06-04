fce99163b324f547f13bb5c73e93b67b4b279ff0
joaquimcosta-dev <joaquimdacosta1805@gmail.com>
2026-06-04 19:57:33 +0100

    feat: front cadastro de produto pegando as categorias do banco

--- frontend/src/views/cadastrarProduto/cadastrarProduto.js
+++ frontend/src/views/cadastrarProduto/cadastrarProduto.js
@@ -1,5 +1,5 @@
 const BASE_URL = "http://localhost:3000/produto";
-const BASER_URL_CAT = "http://localhost:3000/categoria";
+const BASE_URL_CAT = "http://localhost:3000/index/categoria";
 
 let categorias = [];
 let produtos = [];
